2366fbcc7715f7f874ff464153f089fa9079d7f8
tolli-txt <oskartolli@hotmail.com>
2022-09-07 15:55:38 +0200

    Completed exercises for lesson 2

---
+++ lesson_2/Tolli-txt/input.py
@@ -0,0 +1,16 @@
+# 1
+name = input("Enter your name: ")
+print("Hello", name, "have a great day!")
+
+# 2
+int1 = int(input("Enter a number: "))
+print(int1)
+
+# 3
+string1 = input("Write anything: ")
+print(string1 * 2)
+
+# 4
+print("Your float value will be divided by 3.5, your float value needs a decimal.")
+float1 = float(input("Input a float value: "))
+print(float1 / 3.5)
---
+++ lesson_2/Tolli-txt/print.py
@@ -0,0 +1,23 @@
+# 1
+int_value = 10
+print("Static int is", int_value)
+
+# 2
+float_value = 9.5
+print("Static float is", float_value)
+
+# 3
+string = "Test text for testing."
+print(string)
+
+# 4
+boolean_value = True
+print(boolean_value)
+
+# 5
+nothing_value = None
+print(nothing_value)
+
+# 6
+print(f"All values in a row now...", int_value,
+      float_value, string, boolean_value, nothing_value)
